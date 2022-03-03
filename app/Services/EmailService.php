de236aa1716233ebddd31089e09bdf16d84fd5fd
Andelo Mata <andmatboy@gmail.com>
2022-03-03 12:02:36 +0100

    reset password email page added

--- app/Services/EmailService.php
+++ app/Services/EmailService.php
@@ -41,6 +41,25 @@ class EmailService
         $mail->send();
     }
 
+    public function resetPassword($subject, $emailUser, $nameUser, $isHtml, $activation_token)
+    {
+        $mail = new PHPMailer;
+        $mail->isSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->Host = $this->host;
+        $mail->Port = $this->port;
+        $mail->Username = $this->username;
+        $mail->Password = $this->password;
+        $mail->SMTPAuth = true;
+        $mail->Subject = $subject;
+        $mail->setFrom($this->app_name, $this->app_name);
+        $mail->addReplyTo($this->app_name, $this->app_name);
+        $mail->addAddress($emailUser, $nameUser);
+        $mail->isHTML($isHtml);
+        $mail->Body = $this->viewResatPassword($nameUser, $activation_token);
+        $mail->send();
+    }
+
     public function viewSendEmail($name, $activation_code, $activation_token)
     {
         return view('mail.confirmation_email')
@@ -49,5 +68,14 @@ class EmailService
                 'activation_code' => $activation_code,
                 'activation_token' => $activation_token
             ]);
+    }
+
+    public function viewResatPassword($name, $activation_token)
+    {
+        return view('mail.reset_password')
+        ->with([
+            'name' => $name,
+            'activation_token' => $activation_token
+        ]);
     }
 }
--- resources/views/auth/forgot_password.blade.php
+++ resources/views/auth/forgot_password.blade.php
@@ -17,7 +17,7 @@
                 @include('alerts.alert-message')
 
                 <label for="email-send" class="form-label">Email</label>
-                <input type="email" name="email-send" id="email-send" class="form-control" placeholder="Please enter your email address">
+                <input type="email" name="email-send" id="email-send" class="form-control @error('email-error') is-invalid @enderror" placeholder="Please enter your email address" value="@if(Session::has('old_email')) {{ Session::get('old_email') }} @endif" required>
 
                 <div class="d-grid gap-2 mt-4 mb-5">
                     <button class="btn btn-primary" type="submit">Reset password</button>
